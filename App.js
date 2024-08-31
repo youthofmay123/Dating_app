79c8553a70792f40a68e0aecfda5dd5d7dae4aaa
youthofmay123 <nhathuy39337343@gmail.com>
2024-08-31 23:56:18 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi linda xin chào cả nhà</Text>
       <StatusBar style="auto" />
     </View>
   );
