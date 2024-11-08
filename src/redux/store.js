b9b78d27c66321767ea69aeca35ba8cb574ff16d
youthofmay123 <nhathuy39337343@gmail.com>
2024-11-08 16:32:02 +0700

    Final 1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice'; // Import userReducer
 
-// Cấu hình store
-=======
-//Lưu trữ thông tin
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './userSlice';
-
->>>>>>> feature/chat
 const store = configureStore({
     reducer: {
         user: userReducer,
     },
 });
-<<<<<<< HEAD
 
-=======
->>>>>>> feature/chat
 export default store;
